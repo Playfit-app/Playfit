346f83625e7393442b9163fcd68a647954ebd408
Ethan SAUX <eowin.saux@gmail.com>
2024-09-18 22:20:58 +0900

    test: tests login, register and logout routes

--- backend/playfit/authentification/views.py
+++ backend/playfit/authentification/views.py
@@ -41,9 +41,6 @@ class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        try:
-            token = Token.objects.get(user=request.user)
-            token.delete()
-            return Response({'success': "Successfully logged out"}, status=status.HTTP_200_OK)
-        except Token.DoesNotExist:
-            return Response({'error': "Token not found"}, status=status.HTTP_400_BAD_REQUEST)
+        token = Token.objects.get(user=request.user)
+        token.delete()
+        return Response({'success': "Successfully logged out"}, status=status.HTTP_200_OK)

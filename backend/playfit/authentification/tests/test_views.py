13521ab1514db5cb86f8ef683bb29eb6c12e6ddf
Ethan SAUX <eowin.saux@gmail.com>
2025-02-06 11:19:00 +0100

    test: tests views

--- backend/playfit/authentification/tests/test_views.py
+++ backend/playfit/authentification/tests/test_views.py
@@ -1,10 +1,11 @@
-from django.urls import reverse
+from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
-from .models import CustomUser
+from authentification.models import CustomUser
+from authentification.views import ResetPasswordView, AccountRecoveryView
 
-class AuthentificationTests(APITestCase):
+class RegisterViewTests(APITestCase):
     def setUp(self):
         self.user = CustomUser.objects.create_user(
             email="test@test.com",
@@ -14,9 +15,9 @@ class AuthentificationTests(APITestCase):
             height=180,
             weight=80,
         )
+        self.url = "/api/auth/register/"
 
     def test_register(self):
-        url = reverse('register')
         data = {
             'email': 'test2@test.com',
             'username': 'test2',
@@ -28,12 +29,11 @@ class AuthentificationTests(APITestCase):
             'privacy_policy': True,
             'marketing': False,
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(CustomUser.objects.count(), 2)
 
     def test_register_with_existing_email(self):
-        url = reverse('register')
         data = {
             'email': 'test@test.com',
             'username': 'test2',
@@ -45,12 +45,11 @@ class AuthentificationTests(APITestCase):
             'privacy_policy': True,
             'marketing': False,
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(CustomUser.objects.count(), 1)
 
     def test_register_with_existing_username(self):
-        url = reverse('register')
         data = {
             'email': 'test2@test.com',
             'username': 'test',
@@ -62,12 +61,11 @@ class AuthentificationTests(APITestCase):
             'privacy_policy': True,
             'marketing': False,
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(CustomUser.objects.count(), 1)
 
     def test_register_password_too_common(self):
-        url = reverse('register')
         data = {
             'email': 'test2@test.com',
             'username': 'test2',
@@ -79,12 +77,11 @@ class AuthentificationTests(APITestCase):
             'privacy_policy': True,
             'marketing': False,
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(CustomUser.objects.count(), 1)
 
     def test_register_password_too_short(self):
-        url = reverse('register')
         data = {
             'email': 'test2@test.com',
             'username': 'test2',
@@ -96,12 +93,11 @@ class AuthentificationTests(APITestCase):
             'privacy_policy': True,
             'marketing': False,
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(CustomUser.objects.count(), 1)
 
     def test_register_password_too_similar_to_username(self):
-        url = reverse('register')
         data = {
             'email': 'test2@test.com',
             'username': 'test2',
@@ -113,12 +109,11 @@ class AuthentificationTests(APITestCase):
             'privacy_policy': True,
             'marketing': False,
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(CustomUser.objects.count(), 1)
 
     def test_register_with_invalid_date_of_birth(self):
-        url = reverse('register')
         data = {
             'email': 'test2@test.com',
             'username': 'test2',
@@ -130,49 +125,117 @@ class AuthentificationTests(APITestCase):
             'privacy_policy': True,
             'marketing': False,
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(CustomUser.objects.count(), 1)
 
+class LoginViewTests(APITestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email="test@test.com",
+            username="test",
+            password="test12345",
+            date_of_birth="1990-01-01",
+            height=180,
+            weight=80,
+        )
+        self.url = "/api/auth/login/"
+
     def test_login(self):
-        url = reverse('login')
         data = {
             'username': 'test',
             'password': 'test12345'
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('token', response.data)
 
     def test_invalid_login(self):
-        url = reverse('login')
         data = {
             'username': 'test',
             'password': 'invalid'
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_login_with_email(self):
-        url = reverse('login')
         data = {
             'email': 'test@test.com',
             'password': 'test12345'
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('token', response.data)
 
+    def test_invalid_login_with_email(self):
+        data = {
+            'email': 'test@invalid.com',
+            'password': 'test12345'
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class LogoutViewTests(APITestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email="test@test.com",
+            username="test",
+            password="test12345",
+            date_of_birth="1990-01-01",
+            height=180,
+            weight=80,
+        )
+        self.url = "/api/auth/logout/"
+
     def test_logout(self):
-        url = reverse('logout')
         token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
-        response = self.client.post(url)
+        response = self.client.post(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Token.objects.count(), 0)
 
     def test_invalid_logout(self):
-        url = reverse('logout')
-        response = self.client.post(url)
+        response = self.client.post(self.url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(Token.objects.count(), 0)
+
+class UserViewTests(APITestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email="test@test.com",
+            username="test",
+            password="test12345",
+            date_of_birth="1990-01-01",
+            height=180,
+            weight=80,
+        )
+        self.url = "/api/auth/get_my_data/"
+        self.update_url = "/api/auth/update_my_data/"
+        self.delete_url = "/api/auth/delete_my_data/"
+
+    def test_get_my_data(self):
+        token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["email"], "test@test.com")
+        self.assertEqual(response.data["username"], "test")
+        self.assertEqual(response.data["date_of_birth"], "1990-01-01")
+        self.assertEqual(response.data["height"], "180.00")
+        self.assertEqual(response.data["weight"], "80.00")
+
+    def test_update_my_data(self):
+        token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        data = {
+            'username': 'test2',
+        }
+        response = self.client.patch(self.update_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["username"], "test2")
+
+    def test_delete_my_data(self):
+        token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = self.client.delete(self.delete_url, data={'confirm': True}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)        

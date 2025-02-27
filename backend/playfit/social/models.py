3241cc2fc70e71d02857823a4e3828951cf3e458
Ethan SAUX <eowin.saux@gmail.com>
2025-02-27 14:13:16 +0900

    feat: follower / following system for users

--- backend/playfit/social/models.py
+++ backend/playfit/social/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from authentification.models import CustomUser
 
-# Create your models here.
+class Follow(models.Model):
+    follower = models.ForeignKey(CustomUser, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(CustomUser, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("follower", "following")
+
+    def __str__(self):
+        return f"{self.follower} follows {self.following}"
+
+    def clean(self):
+        if self.follower == self.following:
+            raise ValidationError("You can't follow yourself")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
--- backend/playfit/social/views.py
+++ backend/playfit/social/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, get_object_or_404
+from django.contrib.auth import get_user_model
+from .models import Follow
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class FollowersListView(ListAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(following__follower=user)
+
+class FollowingListView(ListAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(followers__following=user)
+
+class FollowCreateView(CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        user_to_follow = get_object_or_404(User, id=request.data.get('id'))
+        if user == user_to_follow:
+            return Response({'detail': 'You cannot follow yourself'}, status=status.HTTP_400_BAD_REQUEST)
+        follow = Follow.objects.filter(follower=user, following=user_to_follow)
+        if follow.exists():
+            return Response({'detail': 'You already follow this user'}, status=status.HTTP_400_BAD_REQUEST)
+        Follow.objects.create(follower=user, following=user_to_follow)
+        return Response({'detail': 'User followed'}, status=status.HTTP_201_CREATED)
+
+class FollowDeleteView(DestroyAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        user = request.user
+        user_to_unfollow = get_object_or_404(User, id=kwargs['id'])
+        follow = get_object_or_404(Follow, follower=user, following=user_to_unfollow)
+        follow.delete()
+        return Response({'detail': 'User unfollowed'}, status=status.HTTP_204_NO_CONTENT)

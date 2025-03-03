af3bbbeb9245009f46ff0b9749b8b4cc19c34c33
Ethan SAUX <eowin.saux@gmail.com>
2025-03-03 12:57:04 +0900

    feat: delete old seen notifications script

---
+++ backend/playfit/social/management/commands/delete_old_notifications.py
@@ -0,0 +1,10 @@
+from datetime import timedelta
+from django.utils.timezone import now
+from django.core.management.base import BaseCommand
+from your_app.models import Notification
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        one_month_ago = now() - timedelta(days=30)
+        notifications_to_delete = Notification.objects.filter(seen=True, created_at__lt=one_month_ago).delete()
+        self.stdout.write(f"{notifications_to_delete} notifications deleted")

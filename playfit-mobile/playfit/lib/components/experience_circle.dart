4fe83595a924b6d595f5dc09f767e9b13d637898
Ethan SAUX <eowin.saux@gmail.com>
2025-03-06 13:11:20 +0900

    feat: beginning profile page

---
+++ playfit-mobile/playfit/lib/components/experience_circle.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class ExperienceCirclePainter extends CustomPainter {
+  final double progress;
+
+  ExperienceCirclePainter(this.progress);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint backgroundPaint = Paint()
+      ..color = Colors.transparent
+      ..strokeWidth = 2
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    Paint progressPaint = Paint()
+      ..color = const Color.fromARGB(255, 115, 145, 253)
+      ..strokeWidth = 2
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    Offset center = Offset(size.width / 2, size.height / 2);
+    double radius = (size.width - 4) / 2;
+
+    canvas.drawCircle(center, radius, backgroundPaint);
+
+    double sweepAngle = 2 * pi * progress;
+    canvas.drawArc(Rect.fromCircle(center: center, radius: radius), -pi / 2,
+        sweepAngle, false, progressPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
+
+class ExperienceCircle extends StatelessWidget {
+  final double currentXP;
+  final double requiredXP;
+
+  const ExperienceCircle({
+    super.key,
+    required this.currentXP,
+    required this.requiredXP,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double progress = (currentXP / requiredXP).clamp(0.0, 1.0);
+
+    return SizedBox(
+      width: 71,
+      height: 71,
+      child: CustomPaint(
+        size: const Size(71, 71),
+        painter: ExperienceCirclePainter(progress),
+      ),
+    );
+  }
+}
--- playfit-mobile/playfit/lib/profile_page.dart
+++ playfit-mobile/playfit/lib/profile_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:playfit/components/statistic_bar.dart';
-import 'package:playfit/styles/styles.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:playfit/components/experience_circle.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -8,33 +8,205 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppStyles.grey,
-      body: SafeArea(
-        child: SingleChildScrollView( // Wrap with scrolling
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Text(
-                'PROFIL',
-                style: AppStyles.usernameRegular,
-              ),
-              const SizedBox(height: 40),
-              Image.asset(
-                'assets/images/character.png',
-                width: 151,
-                height: 297,
-              ),
-              Text(
-                'Hugo',
-                style: AppStyles.usernameRegular,
-              ),
-              const SizedBox(height: 40),
-              const StatisticBar(),
-            ],
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.settings_outlined),
+            color: Colors.black,
+            onPressed: () {},
           ),
-        ),
+        ],
       ),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            top: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/mountains/mont_blanc.png'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Align(
+                alignment: const Alignment(0, -0.3),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 204, 255, 178),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Image.asset(
+                    'assets/images/characterMapFlip.png',
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: MediaQuery.of(context).size.height * 0.3,
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(28.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "Prénom",
+                            style: GoogleFonts.amaranth(fontSize: 36),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 8.0, top: 14.0),
+                            child: Text(
+                              "@identifiant",
+                              style: TextStyle(fontSize: 14),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Membre depuis Septembre 2024",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Color.fromARGB(255, 120, 119, 111),
+                          ),
+                        ),
+                      ),
+                      const Row(
+                        children: [
+                          Text("1", style: TextStyle(fontSize: 14)),
+                          Padding(
+                            padding: EdgeInsets.only(left: 4.0),
+                            child: Text(
+                              "abonnés",
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Color.fromARGB(255, 120, 119, 111),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 8.0),
+                            child: Text("5", style: TextStyle(fontSize: 14)),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 4.0),
+                            child: Text(
+                              "abonnements",
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Color.fromARGB(255, 120, 119, 111),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Stack(
+                            children: [
+                              const ExperienceCircle(
+                                currentXP: 70,
+                                requiredXP: 100,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(6),
+                                child: Container(
+                                  height: 58,
+                                  width: 58,
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      image: AssetImage(
+                                          'assets/images/mountains/mont_blanc.png'),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 50,
+                            child: VerticalDivider(
+                              color: Color(0XFFE57106),
+                              thickness: 1,
+                            ),
+                          ),
+                          const Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Icon(Icons.local_fire_department,
+                                  color: Color(0XFFFF7A00), size: 24),
+                              Padding(
+                                padding: EdgeInsets.only(left: 8.0),
+                                child: Text(
+                                  "5\nDay streak",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Color(0XFF1D1B20),
+                                    fontWeight: FontWeight.w400,
+                                    height: 1.4,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 50,
+                            child: VerticalDivider(
+                              color: Color(0XFFE57106),
+                              thickness: 1,
+                            ),
+                          ),
+                          const Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Icon(Icons.flag_rounded, size: 24),
+                              Padding(
+                                padding: EdgeInsets.only(left: 8.0),
+                                child: Text(
+                                  "2\nVilles finies",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Color(0XFF1D1B20),
+                                    fontWeight: FontWeight.w400,
+                                    height: 1.4,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

16c240adee35fd8a62f2a1c1ed0965efd2e553f6
Masteruuuuu <hugo.martins-gandra@epitech.eu>
2025-04-06 19:00:23 +0900

    feat: camera page new design without working elements

--- playfit-mobile/playfit/lib/camera_page.dart
+++ playfit-mobile/playfit/lib/camera_page.dart
@@ -25,8 +25,9 @@ class _CameraViewState extends State<CameraView> {
     final cameras = await availableCameras();
     _controller = CameraController(
       cameras.firstWhere(
-          (camera) => camera.lensDirection == CameraLensDirection.front),
-      ResolutionPreset.medium,
+        (camera) => camera.lensDirection == CameraLensDirection.front,
+      ),
+      ResolutionPreset.high,
     );
     await _controller?.initialize();
 
@@ -35,9 +36,7 @@ class _CameraViewState extends State<CameraView> {
     }
 
     _controller?.startImageStream((image) async {
-      if (_isDetecting) {
-        return;
-      }
+      if (_isDetecting) return;
 
       _isDetecting = true;
       try {
@@ -54,50 +53,63 @@ class _CameraViewState extends State<CameraView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pose Estimation'),
-      ),
       body: _controller != null && _controller!.value.isInitialized
-          ? Column(
+          ? Stack(
               children: [
-                // Camera preview taking half of the screen
-                Expanded(
-                  flex: 1,
-                  child: CameraPreview(_controller!),
+                Center(
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: SizedBox(
+                      width: _controller!.value.previewSize!.height,
+                      height: _controller!.value.previewSize!.width,
+                      child: CameraPreview(_controller!),
+                    ),
+                  ),
                 ),
-                // Workout count and additional UI
-                Expanded(
-                  flex: 1,
+                Align(
+                  alignment: Alignment.bottomCenter,
                   child: Container(
-                    color: Colors.white,
-                    alignment: Alignment.center,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: ValueListenableBuilder<Map<WorkoutType, int>>(
-                        valueListenable: _workoutAnalyzer.workoutCounts,
-                        builder: (context, workoutCounts, child) {
-                          return Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Squat Count: ${workoutCounts[WorkoutType.squat] ?? 0}',
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                    height: 100,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(20)),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Row(
+                          children: [
+                            Icon(Icons.timer, color: Colors.orange),
+                            SizedBox(width: 5),
+                            Text(
+                              '0:30',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
                               ),
-                              const SizedBox(height: 20),
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Add functionality here
-                                },
-                                child: const Text('Analyze Again'),
+                            ),
+                          ],
+                        ),
+                        const Row(
+                          children: [
+                            Icon(Icons.fitness_center, color: Colors.orange),
+                            SizedBox(width: 5),
+                            Text(
+                              '1',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
                               ),
-                            ],
-                          );
-                        },
-                      ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 50,
+                          child: Image.asset("assets/images/mascot.png"),
+                        ),
+                      ],
                     ),
                   ),
                 ),

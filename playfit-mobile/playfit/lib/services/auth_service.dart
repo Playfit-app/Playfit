2c471ac508f5a676d4dcf83f47be25a0c9f2c27c
Ethan SAUX <84964430+Eowiin@users.noreply.github.com>
2025-05-27 17:52:46 +0900

    feat: new character selection at registration

--- playfit-mobile/playfit/lib/services/auth_service.dart
+++ playfit-mobile/playfit/lib/services/auth_service.dart
@@ -65,9 +65,19 @@ class AuthService {
     double weight,
     bool isConsentGiven,
     bool isMarketingConsentGiven,
-    int index,
+    String? characterImage,
   ) async {
     try {
+      if (email.isEmpty ||
+          username.isEmpty ||
+          password.isEmpty ||
+          dateOfBirth.isEmpty ||
+          height <= 0 ||
+          weight <= 0 ||
+          !isConsentGiven ||
+          (characterImage == null || characterImage.isEmpty)) {
+        return {'status': 'error', 'message': 'All fields are required'};
+      }
       final data = <String, dynamic>{
         'email': email,
         'username': username,
@@ -78,7 +88,7 @@ class AuthService {
         'terms_and_conditions': isConsentGiven,
         'privacy_policy': isConsentGiven,
         'marketing': isMarketingConsentGiven,
-        'character_image_id': index + 1,
+        'character_image': characterImage,
       };
       final response = await http.post(
         Uri.parse('${baseUrl}register/'),

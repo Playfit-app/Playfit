6725fa0ff0f66998e548bbf22755d0816cc3019d
Ethan SAUX <84964430+Eowiin@users.noreply.github.com>
2025-06-18 18:46:00 +0900

    feat: landmark anecdotes translations

--- playfit-mobile/playfit/lib/i18n/strings.g.dart
+++ playfit-mobile/playfit/lib/i18n/strings.g.dart
@@ -4,9 +4,9 @@
 /// To regenerate, run: `dart run slang`
 ///
 /// Locales: 2
-/// Strings: 228 (114 per locale)
+/// Strings: 276 (138 per locale)
 ///
-/// Built on 2025-06-18 at 08:24 UTC
+/// Built on 2025-06-18 at 09:39 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import

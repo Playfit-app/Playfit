65d16d053176ac4f59740ba31337137135403798
Ethan SAUX <84964430+Eowiin@users.noreply.github.com>
2025-06-18 19:43:09 +0900

    fix: key for lyon landmark in translations

--- playfit-mobile/playfit/lib/i18n/strings.g.dart
+++ playfit-mobile/playfit/lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 276 (138 per locale)
 ///
-/// Built on 2025-06-18 at 09:39 UTC
+/// Built on 2025-06-18 at 10:09 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
